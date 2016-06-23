2b5adc6dbcee1e5b37c0d9e7c813dc793c8e2332
modularjon <modularjon@gmail.com>
2016-06-23 11:56:07 -0400

    Edit shell scripts to add variables

--- scripts/sign-in.sh
+++ scripts/sign-in.sh
@@ -2,9 +2,9 @@
 
 curl --include --request POST http://localhost:3000/sign-in \
   --header "Content-Type: application/json" \
-  --data '{
-    "credentials": {
-      "email": "an@example.email",
-      "password": "an example password"
+  --data "{
+    \"credentials\": {
+      \"email\": \"$EMAIL\",
+      \"password\": \"$PASSWORD\"
     }
-  }'
+  }"
