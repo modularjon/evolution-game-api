e83ddaa2870dcb19598182078318f965a9f88b39
modularjon <modularjon@gmail.com>
2016-06-27 10:31:18 -0400

    Add hasmany and belongs to for games and parent
    parent_taxon has_many games
    games belong_to parent_taxon

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
   belongs_to :user, inverse_of: :games
+  belongs_to :parent_taxon, inverse_of: :games
 end
--- app/models/parent_taxon.rb
+++ app/models/parent_taxon.rb
@@ -1,3 +1,4 @@
 class ParentTaxon < ActiveRecord::Base
   has_many :child_taxons, inverse_of: :parent_taxon
+  has_many :games, inverse_of: :parent_taxon
 end
