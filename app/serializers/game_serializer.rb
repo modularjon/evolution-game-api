1740e04d76668e5f2e3e68a0b2e67de9db3904a6
modularjon <modularjon@gmail.com>
2016-06-27 10:34:09 -0400

    add serializer attr for parent and games

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,3 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :game_solved, :user_id
+  attributes :id, :game_solved, :user_id, :parent_taxon
 end
--- app/serializers/parent_taxon_serializer.rb
+++ app/serializers/parent_taxon_serializer.rb
@@ -1,3 +1,3 @@
 class ParentTaxonSerializer < ActiveModel::Serializer
-  attributes :id, :taxon_name, :child_taxons
+  attributes :id, :taxon_name, :child_taxons, :games
 end
