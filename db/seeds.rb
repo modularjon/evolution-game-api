0ec26ed7e4b4f562cd7cb3c6ca1a2c2e035e5484
modularjon <modularjon@gmail.com>
2016-06-28 17:00:40 -0400

    move example data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+%w(jon ben dan quinton).each do |name|
+  email = "#{name}@#{name}.com"
+  next if User.exists? email: email
+  User.create!(email: email,
+               password: 'p',
+               password_confirmation: nil)
+end
+
+reptile = ParentTaxon.create( taxon_name: 'reptile' )
+mammal = ParentTaxon.create( taxon_name: 'mammal' )
+bird = ParentTaxon.create( taxon_name: 'bird' )
+fish = ParentTaxon.create( taxon_name: 'fish' )
+
+%w(snake turtle crocodile dinosaur).each do |name|
+  taxon_name = "#{name}"
+  next if ChildTaxon.exists? taxon_name: taxon_name
+  child = ChildTaxon.create(taxon_name: taxon_name)
+  reptile.child_taxons << child
+end
+
+%w(mouse dog lemur human).each do |name|
+  taxon_name = "#{name}"
+  next if ChildTaxon.exists? taxon_name: taxon_name
+  child = ChildTaxon.create(taxon_name: taxon_name)
+  mammal.child_taxons << child
+end
+
+%w(ostrich finch duck hawk).each do |name|
+  taxon_name = "#{name}"
+  next if ChildTaxon.exists? taxon_name: taxon_name
+  child = ChildTaxon.create(taxon_name: taxon_name)
+  bird.child_taxons << child
+end
+
+%w(shark lamprey flounder parrotfish).each do |name|
+  taxon_name = "#{name}"
+  next if ChildTaxon.exists? taxon_name: taxon_name
+  child = ChildTaxon.create(taxon_name: taxon_name)
+  fish.child_taxons << child
+end
